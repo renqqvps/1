f8c472dc0183e5f341a08c675806f4a1af506405
renqqvps <146714272+renqqvps@users.noreply.github.com>
2023-10-03 13:41:37 +0800

    Create 1.lua

---
+++ 1.lua
@@ -0,0 +1,26 @@
+if name == "老脚本" then 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hdkdksqow/lllllIIIIIIlllllIII/main/%E8%80%81%E8%84%9A%E6%9C%AC"))();
+elseif name == "忍脚本" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ren666yyds666/jm/main/1.lua"))();
+elseif qun == "QQ群:606247334" then
+loadstring(game:HttpGet("https://pastebin.com/raw/LdzPjuXY"))()
+elseif name == "能力战争 旭旭翻译" then
+loadstring(game:HttpGet("https://pastebin.com/raw/YWaNyrLZ"))()
+elseif name == "脚本工具" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/noob616161/czzgj/main/main.lua"))();
+elseif qun == "群号:912826390" then
+loadstring(game:HttpGet("https://pastebin.com/raw/dZQwRM85"))();
+elseif qun == "群号:630235992" then
+loadstring(game:HttpGet("https://pastebin.com/raw/n5WVcw6a"))();
+elseif name == "苗脚本" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CN1919810/miaouuu/main/miaov4.lua"))();
+elseif name == "旭脚本" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xuxuem/xuxu5201314/main/%E6%97%AD%E6%97%AD"))();
+else
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "脚本",
+    Text = "错误链接",
+    Duration = 5,
+})
+end
